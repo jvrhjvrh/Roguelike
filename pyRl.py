21d58c1696e95bf90d7156c83e75cb06d74c6ea8
unknown <jvrodrigueshilario@gmail.com>
2018-08-09 19:38:17 -0300

    RNG da dungeon completo

--- pyRl.py
+++ pyRl.py
@@ -1,4 +1,5 @@
 import tdl
+from random import randint
 
 class GameObject:
     def __init__(self,x,y,char,color):
@@ -29,12 +30,32 @@ class Rect:
         self.x2 = x+w
         self.y2 = y+h
 
+    def center(self):
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return (center_x,center_y)
+
+    def intersect(self,other):
+        #retorna verdadeiro se intersectar com outro retangulo
+        return(self.x1 <= other.x2 and self.x2 >= other.x1 and self.y1 <= other.y2 and self.y2 >= other.y1)
+
+
+FOV_ALGO = "BASIC"
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
+
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 color_dark_wall = (0, 0, 100)
+color_light_wall = (130, 110, 50)
 color_dark_ground = (50, 50, 150)
+color_light_ground = (200, 180, 50)
 my_map = []
 
 def create_room(room):
@@ -47,13 +68,52 @@ def create_room(room):
 
 def make_map():
     global my_map
+    rooms = []
+    num_rooms = 0
 
     my_map = [[Tile(True) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
 
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(50, 15, 10, 15)
-    create_room(room1)
-    create_room(room2)
+    for r in range(MAX_ROOMS):
+        w = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = randint(ROOM_MIN_SIZE,ROOM_MAX_SIZE)
+        x = randint(0, MAP_WIDTH - w - 1)
+        y = randint(0, MAP_HEIGHT - h - 1)
+
+        new_room = Rect(x, y, w, h)
+
+        failed = False
+        for other_room in rooms:
+            if(new_room.intersect(other_room)):
+                failed = True
+                break
+        
+        if not failed:
+
+            create_room(new_room)
+
+            (new_x, new_y) = new_room.center()
+            room_no = GameObject(new_x, new_y, chr(65+num_rooms), (255, 255, 255))
+            objects.insert(0, room_no) #draw early, so monsters are drawn on top
+
+            if(num_rooms == 0):
+                player.x = new_x
+                player.y = new_y
+            else:
+
+                (prev_x, prev_y) = rooms[num_rooms -1].center()
+
+                if(randint(0,1)):
+
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+            
+            rooms.append(new_room)
+            num_rooms += 1
+
 
 def render_all():
     for y in range(MAP_HEIGHT):
@@ -69,6 +129,19 @@ def render_all():
 
     root.blit(con,0,0,SCREEN_WIDTH,SCREEN_HEIGHT,0,0)
 
+def create_h_tunnel(x1,x2,y):
+    global my_map
+
+    for x in range(min(x1,x2),max(x1,x2) +1):
+        my_map[x][y].blocked = False
+        my_map[x][y].block_sight = False
+
+def create_v_tunnel(y1,y2,x):
+    global my_map
+    
+    for y in range(min(y1,y2), max(y1,y2)+1):
+        my_map[x][y].blocked = False
+        my_map[x][y].block_sight = False
 
 def handle_keys():
 
@@ -81,12 +154,16 @@ def handle_keys():
     
     if user_input.key == "UP":
         player.move(0,-1)
+        fov_recompute = True
     elif user_input.key == "DOWN":
         player.move(0,1)
+        fov_recompute = True
     elif user_input.key == "LEFT":
         player.move(-1,0)
+        fov_recompute = True
     elif user_input.key == "RIGHT":
         player.move(1,0)
+        fov_recompute = True
 
 tdl.set_font('arial10x10.png',greyscale = True, altLayout = True)
 root = tdl.init(SCREEN_WIDTH,SCREEN_HEIGHT,title = "Roguelike",fullscreen = False)
@@ -96,8 +173,6 @@ npc = GameObject(SCREEN_WIDTH//2 - 5, SCREEN_HEIGHT//2, '@', (255,255,0))
 objects = [npc, player]
 
 make_map()
-player.x = 25
-player.y = 23
 while not tdl.event.is_window_closed():
     render_all()
     tdl.flush()
